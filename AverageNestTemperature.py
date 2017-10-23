08f00ce4fc628a730ec5c9101f1df1932d6345af
Justine Vandendorpe <justinevandendorpe@hotmail.com>
2017-10-23 17:25:34 +0200

    Clean data and attempt to calculate average daily temperature

--- AverageNestTemperature.py
+++ AverageNestTemperature.py
@@ -14,7 +14,24 @@ os.chdir('/Users/jvddorpe/Desktop/PiedFlycatchers/NestTemperatureFiles')
 
 # Import the first csv file as a start
 import pandas
-V011 = pandas.read_csv('V011.csv', sep = ',', skiprows = 19)
-# How to tell there are only 3 columns?
-V011.dtypes
+V011 = pandas.read_csv('V011.csv', sep = ',', skiprows = 20, header = None, names = ['Date/Time', 'Unit', 'Integer', 'Fractional'], dayfirst = True)
+
+# Merging the 'Integer' and 'Fractional' columns to a 'Temperature' column
+V011['Temperature'] = V011[['Integer', 'Fractional']].apply(lambda x: '.'.join(x.fillna('').map(str)), axis=1)
+V011['Temperature'] = V011['Temperature'].str.strip('.')
+
+# Deleting the columns 'Unit', 'Integer', and 'Fractional'
+V011 = V011.drop(['Unit', 'Integer', 'Fractional'], axis=1)
+
+# Convert the data types to Date and Float
+V011['Date/Time'] = pandas.to_datetime(V011['Date/Time'])
+V011['Temperature'] = pandas.to_numeric(V011['Temperature'])
+
+# Calculate daily average temperature - WIP
+V011 = V011.set_index(['Date/Time'])
+V011.index = pandas.to_datetime(V011.index, unit='s')
+DailyAv = V011.resample('D', how = 'mean')
+
+
 print(pandas.DataFrame.head(V011))
+V011.dtypes
