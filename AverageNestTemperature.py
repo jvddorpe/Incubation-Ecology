d991c85463e7dbc8c35e1679f4a3d987bc14f3e5
Justine Vandendorpe <justinevandendorpe@hotmail.com>
2017-10-24 16:34:43 +0200

    Calculate daily average temperature

--- AverageNestTemperature.py
+++ AverageNestTemperature.py
@@ -14,7 +14,9 @@ os.chdir('/Users/jvddorpe/Desktop/PiedFlycatchers/NestTemperatureFiles')
 
 # Import the first csv file as a start
 import pandas
-V011 = pandas.read_csv('V011.csv', sep = ',', skiprows = 20, header = None, names = ['Date/Time', 'Unit', 'Integer', 'Fractional'], dayfirst = True)
+V011 = pandas.read_csv('V011.csv', sep = ',', skiprows = 20, header = None, names = ['Timestamp', 'Unit', 'Integer', 'Fractional'], index_col=0, parse_dates=True, dayfirst = True)
+print(pandas.DataFrame.head(V011))
+V011.dtypes
 
 # Merging the 'Integer' and 'Fractional' columns to a 'Temperature' column
 V011['Temperature'] = V011[['Integer', 'Fractional']].apply(lambda x: '.'.join(x.fillna('').map(str)), axis=1)
@@ -22,16 +24,17 @@ V011['Temperature'] = V011['Temperature'].str.strip('.')
 
 # Deleting the columns 'Unit', 'Integer', and 'Fractional'
 V011 = V011.drop(['Unit', 'Integer', 'Fractional'], axis=1)
+print(pandas.DataFrame.head(V011))
 
 # Convert the data types to Date and Float
-V011['Date/Time'] = pandas.to_datetime(V011['Date/Time'])
-V011['Temperature'] = pandas.to_numeric(V011['Temperature'])
-
-# Calculate daily average temperature - WIP
-V011 = V011.set_index(['Date/Time'])
-V011.index = pandas.to_datetime(V011.index, unit='s')
-DailyAv = V011.resample('D', how = 'mean')
-
-
-print(pandas.DataFrame.head(V011))
+V011['Temperature'] = V011.Temperature.astype(float)
 V011.dtypes
+
+# Calculate daily average temperature
+DailyAv = V011.resample('D', how = 'mean')
+print(pandas.DataFrame.head(DailyAv))
+
+# Adding a column 'Day/Night'
+
+
+
